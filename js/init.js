c51c0c1624f1ff5c6597cbb2856d085fd143f94f
abraxasrex <jbreasor@gmail.com>
2016-02-06 00:30:48 -0800

    mobile view fix 1 trillion

--- js/init.js
+++ js/init.js
@@ -1,12 +1,7 @@
 (function($){
   $(function(){
 
-    window.onload = function () {
-    if(screen.width <= 767) {
-        var mvp = document.getElementById('myViewport');
-        mvp.setAttribute('content','width=767');
-    }
-}
+  
    ///collapse sidebar //
     $('.button-collapse').sideNav();
 
