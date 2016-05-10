f8b5848fc7920646be95b4ef5567b337edd5ce08
abraxasrex <jbreasor@gmail.com>
2016-05-10 14:25:25 -0700

    media query touches

--- js/animate-bg.js
+++ js/animate-bg.js
@@ -31,7 +31,7 @@ function jsAnimate(){
   if(children.length){
     for(var i = 0; i < children.length; i ++){
       children[i].style.top = parseInt(children[i].style.top.slice(0, -2)) + 1.5 + 'px';
-      if(parseInt(children[i].style.top.slice(0, -2)) > parentHeight + 100 || children.length > 25){
+      if(parseInt(children[i].style.top.slice(0, -2)) > parentHeight + 50 || children.length > 25){
          riverContainer.removeChild(children[i]);
      }
     }
